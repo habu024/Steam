6c08df048516e1ede43f517837aee836949bad5c
Atsushi Hirakawa <hirakawa.atsushi@dena.jp>
2015-01-05 02:06:07 +0900

    add library

---
+++ Assets/App/Scripts/Touch/TouchController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TouchController : MonoBehaviour {
+    [SerializeField] int rayDistance = 1000;
+
+    private Camera cam;
+    private Ray ray;
+    private List<RaycastHit> hitList;
+    private List<GameObject> selectedList;
+    private List<TouchReceiver> touchRecieverList;
+    private List<Vector3> worldPositionList;
+
+    void Awake() {
+        cam = GetComponent<Camera>();
+        Reset();
+    }
+
+    void Update () {
+        GetTouchState();
+    }
+
+    private void GetTouchState() {
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButton(0)) {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray, rayDistance);
+            hitList = new List<RaycastHit>();
+
+            foreach(RaycastHit hit in hits) {
+                if(hit.collider != null) {
+                    hitList.Add(hit);
+                }
+            }
+
+            if(hitList.Count > 0) {
+                bool isSame = true;
+                for(int i = 0, l = selectedList.Count; i < l; i++) {
+                    if(hitList.Count < selectedList.Count
+                        || (hitList[i].collider != null
+                            && selectedList[i] != hitList[i].collider.gameObject)
+                    ) {
+                        isSame = false;
+                        break;
+                    }
+                }
+                if(!isSame && selectedList.Count > 0
+                   && touchRecieverList.Count > 0
+                ) {
+                    for(int i = 0, l = touchRecieverList.Count; i < l; i++) {
+                        touchRecieverList[i].RecieveLeave(worldPositionList[i], Input.mousePosition, i, l);
+                    }
+                    Reset();
+                } else {
+                    Reset();
+                    foreach(RaycastHit hit in hitList) {
+                        GameObject obj = hit.collider.gameObject;
+                        TouchReceiver receiver = obj.GetComponent<TouchReceiver>();
+                        if(receiver != null) {
+                            selectedList.Add(obj);
+                            touchRecieverList.Add(receiver);
+                            worldPositionList.Add(hit.point);
+                        }
+                    }
+                }
+            } else {
+                if(touchRecieverList.Count > 0) {
+                    for(int i = 0, l = touchRecieverList.Count; i < l; i++) {
+                        touchRecieverList[i].RecieveLeave(worldPositionList[i], Input.mousePosition, i, l);
+                    }
+                }
+                Reset();
+            }
+        }
+        if(touchRecieverList.Count < 1
+            || touchRecieverList.Count != worldPositionList.Count) {return;}
+
+        if(Input.GetMouseButtonDown(0)) {
+            for(int i = 0, l = touchRecieverList.Count; i < l; i++) {
+                touchRecieverList[i].RecieveStart(worldPositionList[i], Input.mousePosition, i, l);
+            }
+        }
+        if(Input.GetMouseButton(0)) {
+            for(int i = 0, l = touchRecieverList.Count; i < l; i++) {
+                touchRecieverList[i].RecieveMove(worldPositionList[i], Input.mousePosition, i, l);
+            }
+        }
+        if(Input.GetMouseButtonUp(0)) {
+            for(int i = 0, l = touchRecieverList.Count; i < l; i++) {
+                touchRecieverList[i].RecieveEnd(worldPositionList[i], Input.mousePosition, i, l);
+            }
+            Reset();
+        }
+    }
+
+    private void Reset() {
+        selectedList = new List<GameObject>();
+        touchRecieverList = new List<TouchReceiver>();
+        worldPositionList = new List<Vector3>();
+    }
+}
---
+++ Assets/App/Scripts/Touch/TouchReceiver.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TouchReceiver : MonoBehaviour {
+    TouchData data = new TouchData();
+    Vector3 startWorldPos;
+    float startTime;
+
+    public void RecieveStart(Vector3 worldPos, Vector3 screenPos, int index, int len) {
+        startWorldPos = worldPos;
+        startTime = Time.time;
+
+        data.startPosition = worldPos;
+        data.worldPosition = worldPos;
+        data.screenPosition = screenPos;
+        data.distance = 0.0f;
+        data.distanceX = 0.0f;
+        data.distanceY = 0.0f;
+        data.angle = 0.0f;
+        data.direction = null;
+        data.index = index;
+        data.length = len;
+
+        SendMessage("Touch", data, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void RecieveEnd(Vector3 worldPos, Vector3 screenPos, int index, int len) {
+        data.worldPosition = worldPos;
+        data.screenPosition = screenPos;
+        data.index = index;
+        data.length = len;
+
+        if(Time.time - startTime < 1.0f) {
+            if(data.distance < 0.1f) {
+                SendMessage("Tap", data, SendMessageOptions.DontRequireReceiver);
+            } else {
+                SendMessage("Swipe", data, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+        SendMessage("Release", data, SendMessageOptions.DontRequireReceiver);
+    }
+    
+    public void RecieveLeave(Vector3 worldPos, Vector3 screenPos, int index, int len) {
+        data.worldPosition = worldPos;
+        data.screenPosition = screenPos;
+        data.index = index;
+        data.length = len;
+
+        SendMessage("Leave", data, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void RecieveMove(Vector3 worldPos, Vector3 screenPos, int index, int len) {
+        float disX = worldPos.x - startWorldPos.x;
+        float disY = worldPos.y - startWorldPos.y;
+        float angle = Mathf.Atan2(disY, disX) * 180 / Mathf.PI;
+
+        data.worldPosition = worldPos;
+        data.screenPosition = screenPos;
+        data.distance = Mathf.Sqrt(disX * disX + disY * disY);
+        data.distanceX = disX;
+        data.distanceY = disY;
+        data.angle = angle;
+        data.index = index;
+        data.length = len;
+        if(angle >= 45 && angle <= 135) {
+            data.direction = "up";
+        } else if(angle >= -135 && angle <= -45) {
+            data.direction = "down";
+        } else if(angle > -45 && angle < 45) {
+            data.direction = "right";
+        } else {
+            data.direction = "left";
+        }
+
+        SendMessage("Drag", data, SendMessageOptions.DontRequireReceiver);
+    }
+}
+
+public class TouchData {
+    public Vector3 startPosition;
+    public Vector3 worldPosition;
+    public Vector3 screenPosition;
+    public float distance;
+    public float distanceX;
+    public float distanceY;
+    public float angle;
+    public string direction;
+    public int index;
+    public int length;
+}
