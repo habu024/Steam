de1462d889718afec75102417e8df7d88b726108
Atsushi Hirakawa <hirakawa.atsushi@dena.jp>
2015-01-06 23:11:29 +0900

    create tank controller

---
+++ Assets/App/Scripts/Game/TankController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour {
+    float screenRatio;
+
+    void Start() {
+        screenRatio = Screen.width > Screen.height ? 640f / Screen.height : 640f / Screen.width;
+    }
+    
+    // Update is called once per frame
+    void Update() {
+    
+    }
+    
+    void Touch(TouchData data) {
+        Debug.Log(data.screenPosition * screenRatio);
+    }
+}
